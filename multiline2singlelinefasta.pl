a80abe36c280dee24084e6d93a25d4cea0d0c412
tiendu <52783290+tiendu@users.noreply.github.com>
2022-08-10 13:15:39 +0900

    Add files via upload

---
+++ multiline2singlelinefasta.pl
@@ -0,0 +1,19 @@
+use strict;
+
+my $fasta_path = shift @ARGV;
+my $file_path = $fasta_path;
+$file_path =~ s/.*\///;
+my ($file_name, $file_extension) = $file_path =~ /^(.+)\.([^.]+)$/;
+
+open my $fasta, "<:utf8", $fasta_path or die;
+open my $fasta_single_line, ">:utf8", "${file_name}_singleline.${file_extension}";
+while (<$fasta>) {
+    chomp unless m/\A>(.+)/;
+    if ($. > 1 && m/\A>(.+)/) {
+        $_ =~ s/\A/\n/;
+    };
+    print $fasta_single_line "$_";
+    print $fasta_single_line "\n" if eof;
+};
+close $fasta;
+close $fasta_single_line;
