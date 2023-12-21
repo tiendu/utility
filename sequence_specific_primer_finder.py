5aa4b9859d5e67d7aaf43017d630bf4009ea93c5
Tien Du <52783290+tiendu@users.noreply.github.com>
2023-12-21 20:51:34 +0700

    Update sequence_specific_primer_finder.py

--- sequence_specific_primer_finder.py
+++ sequence_specific_primer_finder.py
@@ -8,17 +8,20 @@ import re
 import argparse
 from functools import partial, lru_cache
 from dataclasses import dataclass
-from itertools import product
+from itertools import product, groupby
 import logging
 from typing import List, Tuple, Any
 
-logging.basicConfig(level=logging.INFO)
+# Setup logging to display messages with INFO level and above
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-@dataclass
+@dataclass(frozen=True)
 class Seq:
     id: str
     sequence: str
     quality: str = ''
+    def __hash__(self):
+        return hash((self.id, self.sequence))
 
 def gen_regex(primer: str) -> str:
     nuc_variants = {
@@ -44,6 +47,7 @@ def gen_regex(primer: str) -> str:
         variants = nuc_variants.get(base, [base])
         regex_pattern += '[' + ''.join(variants) + ']' if len(variants) > 1 else re.escape(base)
     return regex_pattern
+
 """
 @lru_cache(maxsize=None)
 def gen_variants(seq: str) -> List[str]:
@@ -68,109 +72,110 @@ def gen_variants(seq: str) -> List[str]:
     vars_gen = [''.join(var) for var in product(*var_lists)]
     return vars_gen
 """
-@lru_cache(maxsize=None)
+
 def rev_complement(dna_seq: str) -> str:
     comp_bases = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
     rev_comp_seq = ''.join(comp_bases.get(base, base) for base in reversed(dna_seq))
     return rev_comp_seq
 
-def check_primers(seq_obj: Seq, fwd_primer_regex: Any, rev_primer_regex: Any) -> Tuple[str, str, str]:
-    fwd_match = fwd_primer_regex.search(seq_obj.sequence)
-    rev_match = rev_primer_regex.search(rev_complement(seq_obj.sequence))
+def check_primers(sequence: Seq, fwd_primer_regex: Any, rev_primer_regex: Any) -> Tuple[str, str, str]:
+    fwd_match = fwd_primer_regex.search(sequence.sequence)
+    rev_match = rev_primer_regex.search(rev_complement(sequence.sequence))
     if fwd_match and rev_match:
-        return seq_obj.id, fwd_match.group(), rev_match.group()
+        return sequence.id, fwd_match.group(), rev_match.group()
     return None, None, None
 
-def check_batch(seq_batch: List[Seq], fwd_primer_regex: Any, rev_primer_regex: Any) -> List[Tuple[str, str, str]]:
-    return [check_primers(seq_obj, fwd_primer_regex, rev_primer_regex) for seq_obj in seq_batch]
+def check_primers_batch(sequences: List[Seq], fwd_primer_regex: Any, rev_primer_regex: Any) -> List[Tuple[str, str, str]]:
+    return [check_primers(sequence, fwd_primer_regex, rev_primer_regex) for sequence in sequences]
 
-# Function to read file
-def read_file(file: str) -> List[str]:
-    if file.endswith('.gz'):
-        with gzip.open(file, 'rt') as gz_file:
-            return gz_file.readlines()
-    elif file.endswith('.qza'):
-        with zipfile.ZipFile(file) as zip_file:
-            sequences_entry_path = next((entry for entry in zip_file.namelist() if re.search('.*/data/dna-sequences\\.fasta', entry)), None)
-            if sequences_entry_path:
-                with zip_file.open(sequences_entry_path, 'r') as sequences_file:
-                    return sequences_file.readlines()
+def read_sequences_from_file(file_path: str, file_type: str) -> List[Seq]:
+    sequences = []
+
+    # Determine the appropriate file opening mode based on the file extension.
+    opener = gzip.open if file_path.endswith('.gz') else open
+
+    if file_type == "FASTQ":
+        with opener(file_path, 'rt') as fin:
+            groups = groupby(enumerate(fin), key=lambda x: x[0] // 4)
+            for _, group in groups:
+                header_line, sequence_line, _, quality_line = [line.strip() for _, line in group]
+                name = header_line[1:]  # Removing "@" character
+                seq = sequence_line.upper()
+                qual = quality_line.upper()
+                sequences.append(Seq(name, seq, qual))
+    elif file_type == "FASTA":
+        with opener(file_path, 'rt') as fin:
+            faiter = (x[1] for x in groupby(fin, lambda line: line[0] == ">"))
+            for header in faiter:
+                headerstr = next(header).strip()
+                name = headerstr[1:]  # Removing ">" character
+                seq = "".join(s.strip().upper() for s in next(faiter))
+                sequences.append(Seq(name, seq))
+    return sequences
+    
+def write_sequences_to_file(sequences: List[Seq], file_path: str) -> None:
+    with open(file_path, 'w') as f:
+        for seq in sequences:
+            if seq.quality == '':
+                f.write(f">{seq.id}\n{seq.sequence}\n")
             else:
-                logging.error("Sequences file not found in the Qiime2 artifact.")
-                raise FileNotFoundError("Sequences file not found in the Qiime2 artifact.")
-    else:  # Plain text
-        with open(file, 'r') as txt_file:
-            return txt_file.readlines()
+                f.write(f"@{seq.id}\n{seq.sequence}\n+\n{seq.quality}\n")
 
-def process_file(input_path: str, fwd_primer: str, rev_primer: str, num_threads: int) -> List[Tuple[str, str, str]]:
-    logging.info(f"Processing file: {input_path}")
-    seqs_with_id = []
-    if not all((isinstance(fwd_primer, str), isinstance(rev_primer, str))):
-        logging.error("Wrong primers.")
-        raise ValueError("All primers must be provided.")
-
-    if not isinstance(num_threads, int) or num_threads < 1:
-        logging.error("The number of threads must be a positive integer.")
-        raise ValueError("The number of threads must be a positive integer.")
-    # Read the file
+def process_file(input_file: str, output_file: str, file_type: str, fwd_primer: str, rev_primer: str, num_threads: int) -> List[Tuple[str, str, str]]:
+    logging.info(f"Processing file: {input_file}")
     try:
-        file_lines = read_file(input_path)
+        sequences = read_sequences_from_file(input_file, file_type)
     except Exception as e:
-        logging.error(f"An error occurred while reading the file: {str(e)}")
-        return []
+        raise ValueError(f'An error occurred while reading the file: {str(e)}')
+        
     logging.info("Compiling regex patterns.")
     fwd_primer_regex = re.compile(gen_regex(fwd_primer))
     rev_primer_regex = re.compile(gen_regex(rev_primer))
     logging.info("Regex patterns compiled.")
-    is_fastq = any(input_path.endswith(extension) for extension in ['.fastq', '.fq', '.fastq.gz', '.fq.gz'])
-    cur_id = None
-    cur_seq = ""
-    cur_quality = ""
-    for line in file_lines:
-        line = line.strip()
-        if line.startswith(">") or line.startswith("@"):
-            if cur_id and cur_seq:
-                seqs_with_id.append(Seq(cur_id, cur_seq, cur_quality))
-            cur_id = line[1:]
-            cur_seq = ""
-            cur_quality = ""
-        elif is_fastq and line.startswith("+"):
-            cur_quality = next(file_lines).strip()
-        else:
-            cur_seq += line
-    if cur_id and cur_seq:
-        seqs_with_id.append(Seq(cur_id, cur_seq, cur_quality))
-    logging.info("Sequences extracted.")
-    batch_size = -(-len(seqs_with_id) // num_threads)
-    check_batch_with_primers = partial(check_batch, fwd_primer_regex=fwd_primer_regex, rev_primer_regex=rev_primer_regex)
-    seq_batches = [seqs_with_id[i:i+batch_size] for i in range(0, len(seqs_with_id), batch_size)]
+    
+    total_sequences = len(sequences)
+    chunk_size = max(1, total_sequences // num_threads)
+    seq_chunks = [sequences[i:i + chunk_size] for i in range(0, total_sequences, chunk_size)]
+    
+    check_batch_with_primers = partial(check_primers_batch, fwd_primer_regex=fwd_primer_regex, rev_primer_regex=rev_primer_regex)
     logging.info("Checking for primer sequences.")
     with concurrent.futures.ProcessPoolExecutor(max_workers=num_threads) as executor:
-        results = executor.map(check_batch_with_primers, seq_batches)
-        match_ids_primers = [res for batch in results for res in batch if res[0] is not None]
-    logging.info("Primer sequence check completed.")
-    return match_ids_primers
-
-def memory_limit_exceeded(limit_percent=80):
-    mem = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024**3
-    total_memory = os.sysconf('SC_PHYS_PAGES') * os.sysconf('SC_PAGE_SIZE') / 1024**3
-    memory_percent_used = (mem / total_memory) * 100
-    if memory_percent_used > limit_percent:
-        logging.error("Memory limit exceeded. Stopping execution.")
-        sys.exit()
-
+        results = executor.map(check_batch_with_primers, seq_chunks)
+        matched_ids_primers = [res for batch in results for res in batch if res[0] is not None]
+    matched_seqs = []
+    for sequence in sequences:
+        for match in matched_ids_primers:
+            if sequence.id == match[0]: 
+                matched_seqs.append(Seq(f"{sequence.id}_{match[1]}_{match[2]}", sequence.sequence, sequence.quality))
+    write_sequences_to_file(matched_seqs, output_file)
+    	    
 def main():
     parser = argparse.ArgumentParser(description='Process DNA sequences with primers.')
-    parser.add_argument('-i', '--input_path', type=str, help='Path to the input file')
-    parser.add_argument('-f', '--forward_primer', type=str, help='Forward primer sequence')
-    parser.add_argument('-r', '--reverse_primer', type=str, help='Reverse primer sequence')
+    parser.add_argument('-i', '--input_file', required=True, type=str, help='Path to the input file.')
+    parser.add_argument('-o', '--output_file', required=True, type=str, help='Path to the output file.')
+    parser.add_argument('-f', '--forward_primer', required=True, type=str, help='Forward primer sequence')
+    parser.add_argument('-r', '--reverse_primer', required=True, type=str, help='Reverse primer sequence')
     parser.add_argument('-t', '--num_threads', type=int, default=4, help='Number of threads (default: 4)')
     args = parser.parse_args()
-    memory_limit_exceeded(80)
+
+    file_type = ''
+    if any(ext in args.input_file for ext in ['.fastq', '.fq']):
+        file_type = "FASTQ"
+    elif any(ext in args.input_file for ext in ['.fasta', '.fa', '.fna']):
+        file_type = "FASTA"
+    else:
+        raise ValueError(f"Unrecognized file extension for {args.input_file}. Expected FASTA (.fasta, .fa, .fna) or FASTQ (.fastq, .fq).")
+        
+    max_threads = os.cpu_count()
+    if args.num_threads < 1 or args.num_threads > max_threads:
+        logging.warning(f"Invalid number of threads. Adjusting thread count to be between 1 and {max_threads}.")
+        args.num_threads = min(max(args.num_threads, 1), max_threads)
+    if args.num_threads >= len(read_sequences_from_file(args.input_file, file_type)) * 0.1:
+        logging.warning(f'Number of sequences too low. Adjusting thread count to 1.')
+        args.num_threads = 1
+
     try:
-        matching_seqs_ids_primers = process_file(args.input_path, args.forward_primer, args.reverse_primer, args.num_threads)  # Process the file with primers
-        for id, forward_primer, reverse_primer in matching_seqs_ids_primers:
-            print(id, forward_primer, reverse_primer)  # Print the matching sequence ids
+        process_file(args.input_file, args.output_file, file_type, args.forward_primer, args.reverse_primer, args.num_threads)
     except Exception as e:
         logging.error(f"An error occurred during processing: {str(e)}")
 
