8bcbc4359ce557ec116a1c4b91a33b6db07d3f38
Tien Du <52783290+tiendu@users.noreply.github.com>
2024-04-19 11:21:41 +0700

    Update align.py

--- align.py
+++ align.py
@@ -3,6 +3,7 @@ import sys
 from dataclasses import dataclass
 from typing import List, Tuple
 
+
 @dataclass
 class SequenceAlignment:
     sequence1: str
@@ -11,6 +12,9 @@ class SequenceAlignment:
     mismatch: int = -1
     gap_opening: int = -2
     gap_extension: int = -1
+    aligned_sequence1: str = ''
+    aligned_sequence2: str = ''
+    score: int = 0
 
     def set_params(self, 
                    match: int = None, 
@@ -49,49 +53,47 @@ class GlobalAlignment(SequenceAlignment):
                 matrix[i][j] = max(match_score, gap_open_score, gap_extend_score)
 
         # Traceback to find the alignment
-        alignment1 = ''
-        alignment2 = ''
         alignment_symbols = ''
         i, j = rows - 1, cols - 1
         while i > 0 and j > 0:
             if matrix[i][j] == matrix[i - 1][j - 1] + (self.match if self.sequence1[i - 1] == self.sequence2[j - 1] else self.mismatch):
-                alignment1 = self.sequence1[i - 1] + alignment1
-                alignment2 = self.sequence2[j - 1] + alignment2
+                self.aligned_sequence1 = self.sequence1[i - 1] + self.aligned_sequence1
+                self.aligned_sequence2 = self.sequence2[j - 1] + self.aligned_sequence2
                 alignment_symbols = '|' + alignment_symbols if self.sequence1[i - 1] == self.sequence2[j - 1] else ' ' + alignment_symbols
                 i -= 1
                 j -= 1
             elif matrix[i][j] == matrix[i - 1][j] + self.gap_opening:
-                alignment1 = self.sequence1[i - 1] + alignment1
-                alignment2 = '-' + alignment2
+                self.aligned_sequence1 = self.sequence1[i - 1] + self.aligned_sequence1
+                self.aligned_sequence2 = '-' + self.aligned_sequence2
                 alignment_symbols = ' ' + alignment_symbols
                 i -= 1
             elif matrix[i][j] == matrix[i][j - 1] + self.gap_opening:
-                alignment1 = '-' + alignment1
-                alignment2 = self.sequence2[j - 1] + alignment2
+                self.aligned_sequence1 = '-' + self.aligned_sequence1
+                self.aligned_sequence2 = self.sequence2[j - 1] + self.aligned_sequence2
                 alignment_symbols = ' ' + alignment_symbols
                 j -= 1
             elif matrix[i][j] == matrix[i - 1][j - 1] + self.gap_extension:
-                alignment1 = self.sequence1[i - 1] + alignment1
-                alignment2 = self.sequence2[j - 1] + alignment2
+                self.aligned_sequence1 = self.sequence1[i - 1] + self.aligned_sequence1
+                self.aligned_sequence2 = self.sequence2[j - 1] + self.aligned_sequence2
                 alignment_symbols = '|' + alignment_symbols if self.sequence1[i - 1] == self.sequence2[j - 1] else ' ' + alignment_symbols
                 i -= 1
                 j -= 1
 
         # Fill in the rest of the alignment if one sequence is longer than the other
         while i > 0:
-            alignment1 = self.sequence1[i - 1] + alignment1
-            alignment2 = '-' + alignment2
+            self.aligned_sequence1 = self.sequence1[i - 1] + self.aligned_sequence1
+            self.aligned_sequence2 = '-' + self.aligned_sequence2
             alignment_symbols = ' ' + alignment_symbols
             i -= 1
         while j > 0:
-            alignment1 = '-' + alignment1
-            alignment2 = self.sequence2[j - 1] + alignment2
+            self.aligned_sequence1 = '-' + self.aligned_sequence1
+            self.aligned_sequence2 = self.sequence2[j - 1] + self.aligned_sequence2
             alignment_symbols = ' ' + alignment_symbols
             j -= 1
 
-        score = matrix[rows - 1][cols - 1]
+        self.score = matrix[rows - 1][cols - 1]
 
-        return alignment1, alignment_symbols, alignment2, score
+        return self.aligned_sequence1, alignment_symbols, self.aligned_sequence2, self.score
     
 @dataclass
 class LocalAlignment(SequenceAlignment):
@@ -100,7 +102,6 @@ class LocalAlignment(SequenceAlignment):
         cols = len(self.sequence2) + 1
         matrix = [[0] * cols for _ in range(rows)]
         
-        max_score = 0
         max_i = 0
         max_j = 0
 
@@ -111,46 +112,44 @@ class LocalAlignment(SequenceAlignment):
                 gap_extend_score = matrix[i - 1][j - 1] + self.gap_extension
                 matrix[i][j] = max(match_score, gap_open_score, gap_extend_score, 0)
 
-                if matrix[i][j] > max_score:
-                    max_score = matrix[i][j]
+                if matrix[i][j] > self.score:
+                    self.score = matrix[i][j]
                     max_i = i
                     max_j = j
         
-        alignment1 = ''
-        alignment2 = ''
         alignment_symbols = ''
         i, j = max_i, max_j
 
         while i > 0 and j > 0 and matrix[i][j] != 0:
             if matrix[i][j] == matrix[i - 1][j - 1] + (self.match if self.sequence1[i - 1] == self.sequence2[j - 1] else self.mismatch):
-                alignment1 = self.sequence1[i - 1] + alignment1
-                alignment2 = self.sequence2[j - 1] + alignment2
+                self.aligned_sequence1 = self.sequence1[i - 1] + self.aligned_sequence1
+                self.aligned_sequence2 = self.sequence2[j - 1] + self.aligned_sequence2
                 alignment_symbols = '|' + alignment_symbols if self.sequence1[i - 1] == self.sequence2[j - 1] else ' ' + alignment_symbols
                 i -= 1
                 j -= 1
             elif matrix[i][j] == matrix[i - 1][j] + self.gap_opening:
-                alignment1 = self.sequence1[i - 1] + alignment1
-                alignment2 = '-' + alignment2
+                self.aligned_sequence1 = self.sequence1[i - 1] + self.aligned_sequence1
+                self.aligned_sequence2 = '-' + self.aligned_sequence2
                 alignment_symbols = ' ' + alignment_symbols
                 i -= 1
             elif matrix[i][j] == matrix[i][j - 1] + self.gap_opening:
-                alignment1 = '-' + alignment1
-                alignment2 = self.sequence2[j - 1] + alignment2
+                self.aligned_sequence1 = '-' + self.aligned_sequence1
+                self.aligned_sequence2 = self.sequence2[j - 1] + self.aligned_sequence2
                 alignment_symbols = ' ' + alignment_symbols
                 j -= 1
             elif matrix[i][j] == matrix[i - 1][j - 1] + self.gap_extension:
-                alignment1 = self.sequence1[i - 1] + alignment1
-                alignment2 = self.sequence2[j - 1] + alignment2
+                self.aligned_sequence1 = self.sequence1[i - 1] + self.aligned_sequence1
+                self.aligned_sequence2 = self.sequence2[j - 1] + self.aligned_sequence2
                 alignment_symbols = '|' + alignment_symbols if self.sequence1[i - 1] == self.sequence2[j - 1] else ' ' + alignment_symbols
                 i -= 1
                 j -= 1
 
-        return alignment1, alignment_symbols, alignment2, max_score
+        return self.aligned_sequence1, alignment_symbols, self.aligned_sequence2, self.score
 
-    def find_local_alignment_location(self, alignment1, alignment2) -> List[int]:
+    def find_local_alignment_location(self) -> List[int]:
         # Remove gap characters from the aligned sequences
-        aligned_sequence1_without_gaps = alignment1.replace('-', '')
-        aligned_sequence2_without_gaps = alignment2.replace('-', '')
+        aligned_sequence1_without_gaps = self.aligned_sequence1.replace('-', '')
+        aligned_sequence2_without_gaps = self.aligned_sequence2.replace('-', '')
 
         # Find the starting position of the aligned sequence in the original sequence
         match1 = re.search(aligned_sequence1_without_gaps, self.sequence1)
@@ -169,7 +168,7 @@ def main(sequence1: str, sequence2: str) -> None:
     local_alignment = LocalAlignment(sequence1, sequence2)
     local_alignment.set_params(match=3, mismatch=-2, gap_opening=-3, gap_extension=-2)
     local_aligned_sequence1, local_alignment_symbols, local_aligned_sequence2, local_score = local_alignment.align()
-    local_alignment_position1, local_alignment_position2 = local_alignment.find_local_alignment_location(local_aligned_sequence1, local_aligned_sequence2)
+    local_alignment_position1, local_alignment_position2 = local_alignment.find_local_alignment_location()
 
     print('Global Alignment\n')
     print(global_aligned_sequence1)
