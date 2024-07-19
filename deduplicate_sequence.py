3041413ce986a14faf48e836f681472202b4d2d3
Tien Du <52783290+tiendu@users.noreply.github.com>
2024-07-19 16:48:05 +0700

    Update deduplicate_sequence.py

--- deduplicate_sequence.py
+++ deduplicate_sequence.py
@@ -69,11 +69,6 @@ def generate_kmers(string: str, k: int) -> Generator[str, None, None]:
     for i in range(len(string) - k + 1):
         yield string[i:i+k]
 
-def round_robin_divide(sequences: List[Seq], chunk_size: int, num_threads: int) -> Generator[List[Seq], None, None]:
-    chunks = [sequences[i:i + chunk_size] for i in range(0, len(sequences), chunk_size)]
-
-    return [chunks[i % num_threads] for i in range(len(chunks))]
-
 def hash_sequence(sequence: str, hash_function=hashlib.sha3_256) -> str:
     return hash_function(sequence.encode()).hexdigest()
 
@@ -99,14 +94,14 @@ def deduplicate_concurrently(sequences: List[Seq], num_threads: int) -> List[Seq
     while sequences:
         total_sequences = len(sequences)
         logging.info(f'Current number of sequences: {total_sequences}')
-        shared_sequences: Dict[str, Seq] = dict()
-        shared_kmers: Set[str] = set()
+        shared_sequences = dict()  # type: Dict[str, Seq]
+        shared_kmers = set()       # type: Set[str]
         sequences = sorted(sequences, key=lambda sequence: sequence.length(), reverse=True)
         min_length = sequences[-1].length()
-        divided_chunks = round_robin_divide(sequences, chunk_size, num_threads)
-        sequences.clear()
+        divided_chunks = [[sequences[i:i + chunk_size] for i in range(0, total_sequences, chunk_size)][j::num_threads] for j in range(num_threads)]
+        # sequences.clear()
 
-        with concurrent.futures.ProcessPoolExecutor(max_workers=num_threads) as executor:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
             func = partial(deduplicate_chunk, uniq_seqs=shared_sequences, uniq_kmers=shared_kmers, min_length=min_length)
             futures = [executor.submit(func, chunk) for chunks in divided_chunks for chunk in chunks]
             concurrent.futures.wait(futures)
