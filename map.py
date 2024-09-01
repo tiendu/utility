8c42c08674de1e73773147c656640519afa08d39
Tien Du <52783290+tiendu@users.noreply.github.com>
2024-09-01 21:21:35 +0700

    Update map.py

--- map.py
+++ map.py
@@ -3,20 +3,20 @@ from typing import List, Tuple, Generator
 from dataclasses import dataclass
 import concurrent.futures
 from functools import partial
-from itertools import groupby, product
+from itertools import product
+from math import sqrt
 import os
 import csv
 import gzip
 from pathlib import Path
-from collections import Counter
-from math import sqrt
 import argparse
+import sys
 
 # Constants
 FASTQ_EXTENSIONS = ['.fastq', '.fq']
 FASTA_EXTENSIONS = ['.fasta', '.fa', '.fna', '.faa']
 
-@dataclass(frozen=True)
+@dataclass(frozen=False)
 class Seq:
     id: str
     sequence: str
@@ -73,6 +73,7 @@ def reverse_translate(sequence: str) -> str:
         'P': 'CCN', 'T': 'ACN', 'V': 'GTN', 'I': 'ATH',
         '*': 'TRR', 'X': 'NNN'
     }
+
     return ''.join(aa_to_nu.get(aa, 'NNN') for aa in sequence)
 
 def delineate(dna: str) -> List[str]:
@@ -91,6 +92,15 @@ def delineate(dna: str) -> List[str]:
     
     return variants
 
+def expand_sequences(sequences: List[Seq]) -> List[Seq]:
+    expanded_sequences = []
+
+    for seq in sequences:
+        delineated_seqs = delineate(seq.sequence)
+        expanded_sequences.extend([Seq(seq.id, variant) for variant in delineated_seqs])
+
+    return expanded_sequences
+
 def hash_string(string: str, hash_function=hashlib.sha3_256) -> str:
     return hash_function(string.encode()).hexdigest()
 
@@ -98,40 +108,42 @@ def generate_hashed_kmers(string: str, k: int) -> Generator[str, None, None]:
     for i in range(len(string) - k + 1):
         yield hash_string(string[i:i + k])
 
-def calculate_similarity_and_coverage(query_kmers: Counter, reference_kmers: Counter) -> Tuple[float, float]:
-    common_kmer_count = sum(min(query_kmers[kmer], reference_kmers.get(kmer, 0)) for kmer in query_kmers)
-    query_unique_count = sum(query_kmers[kmer] for kmer in query_kmers if kmer not in reference_kmers)
-    reference_total_count = sum(reference_kmers.values())
+def cosine_similarity_ordered(query_kmers: List[str], reference_kmers: List[str]) -> float:
+    dot_product = sum(1 for q_kmer, r_kmer in zip(query_kmers, reference_kmers) if q_kmer == r_kmer)
+    norm_query = sqrt(len(query_kmers))
+    norm_reference = sqrt(len(reference_kmers))
     
-    if common_kmer_count == 0:
-        return 0.0, 0.0
+    if norm_query == 0 or norm_reference == 0:
+        return 0.0
     
-    similarity = (common_kmer_count - query_unique_count) / common_kmer_count
-    coverage = common_kmer_count / reference_total_count
-    
-    return similarity, coverage
+    return dot_product / (norm_query * norm_reference)
 
-def map_query_to_reference(query: Seq, reference: Seq, threshold: float) -> List[Tuple[str, str, float, float]]:
+def map_query_to_reference(query: Seq, reference: Seq, similarity_threshold: float, coverage_threshold: float) -> List[Tuple[str, str, str, float, float]]:
     k = max(len(query.sequence) // 5 | 1, 3)
-    query_kmers = Counter(generate_hashed_kmers(query.sequence, k))
-    reference_kmers = Counter(generate_hashed_kmers(reference.sequence, k))
-    
-    similarity, coverage = calculate_similarity_and_coverage(query_kmers, reference_kmers)
-    
-    if similarity >= threshold:
-        return [(query.id, reference.id, similarity, coverage)]
-    
-    return []
+    query_kmers = list(generate_hashed_kmers(query.sequence, k))
+    results = []
+
+    for i in range(len(reference.sequence) - len(query.sequence) + 1):
+        subref = reference.sequence[i:i + len(query.sequence)]
+        subref_kmers = list(generate_hashed_kmers(subref, k))
+        similarity = cosine_similarity_ordered(query_kmers, subref_kmers)
+        coverage = len(subref) / len(reference.sequence)
+
+        if similarity >= similarity_threshold and coverage >= coverage_threshold:
+            results.append((query.id, reference.id, f'{i}..{i+len(query.sequence)}', similarity, coverage))
+
+    return results 
 
 def process_concurrently(query_sequences: List[Seq], 
                          reference_sequences: List[Seq], 
                          similarity_threshold: float, 
+                         coverage_threshold: float, 
                          output_file: str) -> None:
     results = []
 
-    max_workers = os.cpu_count() or 1
+    max_workers = os.cpu_count() - 1 or 1
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
-        func = partial(map_query_to_reference, threshold=similarity_threshold)
+        func = partial(map_query_to_reference, similarity_threshold=similarity_threshold, coverage_threshold=coverage_threshold)
         futures = [executor.submit(func, query, reference) for query, reference in product(query_sequences, reference_sequences)]
         for future in concurrent.futures.as_completed(futures):
             result = future.result()
@@ -139,7 +151,7 @@ def process_concurrently(query_sequences: List[Seq],
                 results.extend(result)
 
     if results:
-        headers = ['Query_ID', 'Reference_ID', 'Similarity', 'Coverage']
+        headers = ['Query_ID', 'Reference_ID', 'Region', 'Similarity', 'Coverage']
         with open(output_file, 'w', newline='') as csvfile:
             csv_writer = csv.writer(csvfile)
             csv_writer.writerow(headers)
@@ -152,6 +164,7 @@ def main():
     parser.add_argument("--query_type", choices=["nu", "aa"], required=True, help="Type of the query input file (nu for nucleotide, aa for amino acid)")
     parser.add_argument("--reference_type", choices=["nu", "aa"], required=True, help="Type of the reference input file (nu for nucleotide, aa for amino acid)")
     parser.add_argument("-t", "--threshold", type=float, default=0.5, help="Similarity threshold for sequence matching (default: 0.5)")
+    parser.add_argument("-c", "--coverage", type=float, default=0.0, help="Coverage threshold for sequence matching (default: 0.0)")
     parser.add_argument("-o", "--output", required=True, help="Path to the output CSV file")
     
     args = parser.parse_args()
@@ -162,11 +175,15 @@ def main():
     if not query_sequences or not reference_sequences:
         print("Error: One or both input files are empty.")
         sys.exit(1)
-    
-    query_sequences.sort(key=lambda x: len(x.sequence), reverse=True)
-    reference_sequences.sort(key=lambda x: len(x.sequence), reverse=True)
-    
-    process_concurrently(query_sequences, reference_sequences, args.threshold, args.output)
+
+    if args.query_type == 'aa':
+        query_sequences = [Seq(seq.id, reverse_translate(seq.sequence)) for seq in query_sequences]
+    if args.reference_type == 'aa':
+        reference_sequences = [Seq(seq.id, reverse_translate(seq.sequence)) for seq in reference_sequences]
+
+    query_sequences = expand_sequences(query_sequences)
+    reference_sequences = expand_sequences(reference_sequences)
+    process_concurrently(query_sequences, reference_sequences, args.threshold, args.coverage, args.output)
 
 if __name__ == "__main__":
     main()
