46e17a392ff3c98b2d78ebc2a85380d84cd4614f
Tien Du <52783290+tiendu@users.noreply.github.com>
2024-09-05 10:43:17 +0700

    Update deduplicate_sequence.py

--- deduplicate_sequence.py
+++ deduplicate_sequence.py
@@ -56,6 +56,7 @@ def read_sequences_from_file(file_path: str, file_type: str) -> List[Seq]:
 
 def write_sequences_to_file(sequences: List[Seq], file_path: str) -> None:
     opener = gzip.open if file_path.endswith('.gz') else open
+
     with opener(file_path, 'wt') as f:
         for seq in sequences:
             if seq.quality == '':
@@ -85,10 +86,12 @@ def round_robin_divide(items: List[Any], chunk_size: int, num_threads: int, key:
         items = sorted(items, key=key, reverse=is_descending)
 
     chunks = [[] for _ in range(num_threads)]
+
     for i, item in enumerate(items):
         chunks[i % num_threads].append(item)
 
     divided_chunks = []
+
     for chunk in chunks:
         for i in range(0, len(chunk), chunk_size):
             divided_chunks.append(chunk[i:i + chunk_size])
@@ -101,8 +104,10 @@ def deduplicate_chunk(sequences: List[Seq], uniq_kmers: Set[str], k: int) -> Lis
 
     for sequence in sequences:
         kmer_hashes = set(hash_string(kmer) for kmer in generate_kmers(sequence.sequence, k))
+
         if all(kmer_hash in uniq_kmers or kmer_hash in local_uniq_kmers for kmer_hash in kmer_hashes):
             continue
+
         local_uniq_seqs[hash_string(sequence.sequence)] = sequence
         local_uniq_kmers.update(kmer_hashes)
 
@@ -120,9 +125,8 @@ def deduplicate_concurrently(sequences: List[Seq], num_threads: int) -> List[Seq
         sequences = sorted(sequences, key=lambda sequence: sequence.length(), reverse=True)
         min_length = sequences[-1].length()
         chunks = round_robin_divide(sequences, chunk_size, num_threads, key=lambda sequence: sequence.length(), is_descending=True)
-
         with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
-            func = partial(deduplicate_chunk, uniq_kmers=shared_kmers, min_length=min_length)
+            func = partial(deduplicate_chunk, uniq_kmers=shared_kmers, k=min_length)
             futures = [executor.submit(func, chunk) for chunk in chunks]
             concurrent.futures.wait(futures)
             for future in concurrent.futures.as_completed(futures):
@@ -130,10 +134,12 @@ def deduplicate_concurrently(sequences: List[Seq], num_threads: int) -> List[Seq
 
         if len(shared_sequences) == len(sequences):
             break
+
         sequences = list(shared_sequences)
         random.shuffle(sequences)
 
     logging.info(f'Final number of sequences: {len(shared_sequences)}')
+    
     return list(shared_sequences)
 
 def main() -> None:
@@ -145,6 +151,7 @@ def main() -> None:
 
     if not os.path.exists(args.input_file):
         raise ValueError(f'The input file "{args.input_file}" does not exist.')
+
     try:
         with open(args.output_file, 'w'):
             pass
@@ -152,6 +159,7 @@ def main() -> None:
         raise ValueError(f'Error creating output file "{args.output_file}": {e}')
 
     file_type = ''
+
     if any(ext in args.input_file for ext in FASTQ_EXTENSIONS):
         file_type = 'FASTQ'
     elif any(ext in args.input_file for ext in FASTA_EXTENSIONS):
@@ -160,11 +168,13 @@ def main() -> None:
         raise ValueError(f'Unrecognized file extension for {args.input_file}. Expected FASTA (.fasta, .fa, .fna) or FASTQ (.fastq, .fq).')
 
     max_threads = os.cpu_count()
+
     if args.num_threads < 1 or args.num_threads > max_threads:
         logging.warning(f'Invalid number of threads. Adjusting thread count to be between 1 and {max_threads}')
         args.num_threads = min(max(args.num_threads, 1), max_threads)
 
     sequences = read_sequences_from_file(args.input_file, file_type)
+
     if not sequences:
         raise ValueError('No sequences detected!')
 
