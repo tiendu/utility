1440467b399d580494bb07a3c13532e6398015f3
Tien Du <52783290+tiendu@users.noreply.github.com>
2024-09-30 12:07:38 +0700

    Update deduplicate_sequence.py

--- deduplicate_sequence.py
+++ deduplicate_sequence.py
@@ -62,18 +62,11 @@ def write_sequences_to_file(sequences: List[Seq], file_path: str) -> None:
             else:
                 f.write(f'@{seq.id}\n{seq.sequence}\n+\n{seq.quality}\n')
 
-def kmerize(string: str, k: int, modifier=None) -> list[str]:
-    if len(string) < k:
-        return []
-    if modifier:
-        return [modifier(string[i:i+k]) for i in range(len(string) - k + 1)]
-    return [string[i:i+k] for i in range(len(string) - k + 1)]
-
 def hash_string(string: str, hash_function=hashlib.sha3_256) -> str:
     return hash_function(string.encode()).hexdigest()
 
-def sequence_to_bits(dna: str) -> list:
-    nucl_to_bits = {
+def dna_to_bits(dna: str) -> list:
+    nu_to_bits = {
         'A': 0b0001,  # A = 1
         'T': 0b0010,  # T = 2
         'G': 0b0100,  # G = 4
@@ -90,38 +83,37 @@ def sequence_to_bits(dna: str) -> list:
         'V': 0b1101,  # V = A | C | G = 13
         'N': 0b1111   # N = A | C | G | T = 15 (any nucleotide)
     }
-    return [nucl_to_bits[nu.upper()] for nu in dna]
+    return [nu_to_bits[nu.upper()] for nu in dna]
 
-def compare_two_strings(str1: str, str2: str, k: int=3) -> bool:
-    if not k or k > min(len(str1), len(str2)):
-        k = min(len(str1), len(str2))
-    bits1 = kmerize(str1, k, sequence_to_bits)
-    bits2 = kmerize(str2, k, sequence_to_bits)
-    def compare_bit_sets(set1: list, set2: list) -> bool:
-        return all(bit1 & bit2 for bit1, bit2 in zip(set1, set2))
+def compare_two_dnas(dna1: str, dna2: str) -> bool:
+    def compare_bits(bits1: list, bits2: list) -> bool:
+        return all(bit1 & bit2 for bit1, bit2 in zip(bits1, bits2))
 
-    if not bits1 or not bits2:
-        return False  # Return False for consistency in boolean values
-    return any(compare_bit_sets(bit1, bit2)
-               for bit1 in bits1 for bit2 in bits2)
+    long = dna1 if len(dna1) > len(dna2) else dna2
+    short = dna1 if len(dna1) <= len(dna2) else dna2
+    long_bits = dna_to_bits(long)
+    short_bits = dna_to_bits(short)
+    for i in range(len(long) - len(short) + 1):
+        if compare_bits(long_bits[i:i + len(short)], short_bits):
+            return True
+    return False
 
-def deduplicate_chunk(sequences: List[str], mode: str) -> List[str]:
-    sequences = sorted(sequences, key=lambda seq: seq.length(), reverse=True)
-    k = sequences[-1].length()  # Shortest sequence length
+def deduplicate_chunk(seqs: List[Seq], mode: str) -> Dict[str, Seq]: 
+    seqs = sorted(seqs, key=lambda seq: seq.length(), reverse=True)
     uniq_seqs = {}
-    uniq_kmers = set()
-    for seq in sequences:
-        kmer_hashes = set(hashed_kmer for hashed_kmer in kmerize(seq.sequence, k, hash_string))
-        if all(kmer_hash in uniq_kmers for kmer_hash in kmer_hashes):
-            continue
-        if mode == 'nu':
-             if not any(compare_two_strings(seq.sequence, unique_seq.sequence, k)
-                        for unique_seq in uniq_seqs.values()):
-                uniq_seqs[hash_string(seq.sequence)] = seq
-                uniq_kmers.update(kmer_hashes)
-        elif mode == 'aa':
+    def is_duplicate(seq: Seq, compare_func) -> bool:
+        for uniq_seq in uniq_seqs.values():
+            if compare_func(uniq_seq.sequence, seq.sequence):
+                return True
+        return False
+
+    if mode == 'nu':  # Nucleotide mode
+        compare_func = compare_two_dnas
+    elif mode == 'aa':  # Amino acid mode
+        compare_func = lambda s1, s2: s1 in s2 or s2 in s1
+    for seq in seqs:
+        if not is_duplicate(seq, compare_func):
             uniq_seqs[hash_string(seq.sequence)] = seq
-            uniq_kmers.update(kmer_hashes)
     return uniq_seqs
 
 def deduplicate_concurrently(sequences: List[Seq], num_threads: int, mode: str) -> List[Seq]:
@@ -136,8 +128,7 @@ def deduplicate_concurrently(sequences: List[Seq], num_threads: int, mode: str) 
             futures = [executor.submit(func, chunk) for chunk in chunks]
             wait(futures)
             for future in as_completed(futures):
-                uniq_seqs = future.result()
-                deduped_seqs.update(uniq_seqs)
+                deduped_seqs.update(future.result())
         if len(deduped_seqs) == len(sequences):
             break
         sequences = list(deduped_seqs.values())
@@ -167,7 +158,7 @@ def main() -> None:
     elif any(ext in args.input_file for ext in FASTA_EXTENSIONS):
         file_type = 'FASTA'
     else:
-        raise ValueError(f'Unrecognized file extension for {args.input_file}. Expected FASTA (.fasta, .fa, .fna) or FASTQ (.fastq, .fq).')
+        raise ValueError(f'Unrecognized file extension for {args.input_file}. Expected FASTA (.fasta, .fa, .fna, .faa) or FASTQ (.fastq, .fq).')
     max_threads = os.cpu_count()
     if args.num_threads < 1 or args.num_threads > max_threads:
         logging.warning(f'Invalid number of threads. Adjusting thread count to be between 1 and {max_threads}')
@@ -183,3 +174,4 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
+
