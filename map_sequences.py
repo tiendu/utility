c23e8740ec3a5fee1665284c8801571436d886dd
Tien Du <52783290+tiendu@users.noreply.github.com>
2024-10-14 10:46:26 +0700

    Update map_sequences.py

--- map_sequences.py
+++ map_sequences.py
@@ -1,3 +1,4 @@
+
 from dataclasses import dataclass
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from itertools import product
@@ -138,8 +139,12 @@ def map_short_to_long(short: Seq,
                       long: Seq,
                       sim_thres: float,
                       cov_thres: float,
-                      is_nucl: bool) -> list[tuple] | None:
-    short, long = (long, short) if short.length() > long.length() else (short, long)
+                      is_nucl: bool,
+                      is_circ: bool) -> list[tuple] | None:
+    if is_circ and short.length() * 2 > long.length():
+        short, long = long, short 
+    elif short.length() > long.length():
+        short, long = long, short
     MatchResult = namedtuple('MatchResult', ['short_id', 'long_id', 'region', 'mismatch', 'similarity', 'coverage', 'strand'])
     max_mismatch = short.length() - round(sim_thres * short.length())
     coverage = short.length() / long.length()
@@ -148,17 +153,30 @@ def map_short_to_long(short: Seq,
     results = set()
     def process_matches(matches, strand, sequence, other_matches=None):
         for region, mm, score in matches:
+            start, end = map(int, region.split('..'))
+            if end > long.length() and start < long.length():
+                end -= long.length()
+                region = f'{start}..{end} (circular)'
+            elif start > long.length():
+                continue
             match = MatchResult(short.id, long.id, region, mm, score, coverage, strand)
             if other_matches:
                 for o_region, o_mm, o_score in other_matches:  # Compare with reverse complement matches if available
+                    o_start, o_end = map(int, o_region.split('..'))
+                    if o_end > long.length() and o_start < long.length():
+                        o_end -= long.length()
+                        o_region = f'{o_start}..{o_end} (circular)'
+                    elif o_start > long.length():
+                        continue
                     if o_score > score and o_mm < mm:
                         match = MatchResult(short.id, long.id, o_region, o_mm, o_score, coverage, '-' if strand == '+' else '+')
             results.add(match)
     
     if is_nucl:
         rc_short = reverse_complement(short.sequence)
-        fw_matches = search_with_kmer_index(long.sequence, short.sequence, dna_to_bits, max_mismatch)
-        rc_matches = search_with_kmer_index(long.sequence, rc_short, dna_to_bits, max_mismatch)
+        long_sequence = long.sequence if not is_circ else long.sequence + long.sequence
+        fw_matches = search_with_kmer_index(long_sequence, short.sequence, dna_to_bits, max_mismatch)
+        rc_matches = search_with_kmer_index(long_sequence, rc_short, dna_to_bits, max_mismatch)
         if fw_matches:
             process_matches(fw_matches, '+', short.sequence, rc_matches)
         elif rc_matches:
@@ -177,6 +195,7 @@ def map_sequences(query_sequences: list[Seq],
                   similarity_threshold: float,
                   coverage_threshold: float,
                   is_nucleotide: bool,
+                  is_circular: bool,
                   output_file: str,
                   num_threads: int) -> None:
     results = []
@@ -185,7 +204,8 @@ def map_sequences(query_sequences: list[Seq],
         func = partial(map_short_to_long,
                        sim_thres=similarity_threshold,
                        cov_thres=coverage_threshold,
-                       is_nucl=is_nucleotide)
+                       is_nucl=is_nucleotide,
+                       is_circ=is_circular)
         futures = [executor.submit(func, query, reference)
                    for query, reference in product(query_sequences, reference_sequences)]
         for future in as_completed(futures):
@@ -239,12 +259,15 @@ def main():
     parser.add_argument('-o', '--output', required=True, help='Path to the output CSV file')
     parser.add_argument('-m', '--mode', choices=['nu', 'aa'], default='nu',
                         help='Comparison mode: "nu" for DNA/RNA, "aa" for proteins (default: "nu")')
+    parser.add_argument('--topology', choices=['linear', 'circular'], default='linear',
+                        help='Topology: "linear"/"circular" for DNA, "linear" for proteins (default: "linear")')
     args = parser.parse_args()
     query_seqs = read_sequences(Path(args.query))
     ref_seqs = read_sequences(Path(args.reference))
     if not query_seqs or not ref_seqs:
         raise ValueError('Error: One or both input files are empty.')
     is_nucleotide = True if args.mode == 'nu' else False
+    is_circular = True if is_nucleotide and args.topology == 'circular' else False
     max_threads = os.cpu_count()
     if args.threads < 1 or args.threads > max_threads:
         logging.warning(f'Invalid number of threads. Adjusting thread count to be between 1 and {max_threads}')
@@ -252,7 +275,7 @@ def main():
     if not (0 <= args.similarity < 1):
         raise ValueError('Similarity threshold should be between 0 (inclusive) and 1 (exclusive)')
     map_sequences(query_seqs, ref_seqs, args.similarity, args.coverage,
-                  is_nucleotide, args.output, args.threads)
+                  is_nucleotide, is_circular, args.output, args.threads)
 
 if __name__ == '__main__':
     main()
